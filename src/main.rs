0fac2b09fa7684dcb5eef7913bc5eb35fe900b2f
Ednan R. Frizzera Filho <ednan@frizzera.dev>
2023-11-06 18:18:27 -0300

    add if statement

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use std::io;
+use std::{io, num};
 
 fn main() {
     println!("Temperature converter");
-    println!("Choose conversion type: 1. [C->F] or 2. [F->C]");
-    let mut conversion_type = String::new();
+    loop {
+        println!("Choose conversion type: 1. [C->F] or 2. [F->C]");
+        let mut conversion_type = String::new();
+        io::stdin()
+            .read_line(&mut conversion_type)
+            .expect("Please, type 1 or 2 to choose a conversion type!");
+        let conversion_type: i32 = match conversion_type.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    io::stdin()
-        .read_line(&mut conversion_type)
-        .expect("Please, type 1 or 2 to choose a conversion type!");
-
-    println!("The number is {conversion_type}");
+        if conversion_type == 1 {
+            println!("chose number 1")
+        } else if conversion_type == 2 {
+            println!("chose number 2")
+        } else {
+            println!("Please choose option 1 or 2!")
+        }
+    }
 }
